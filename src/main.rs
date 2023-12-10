8ff271f7ae299a31bef721f15ecb7e5027b929f0
renandelgadoc <emaildorenan2@gmail.com>
2023-12-10 02:12:15 -0300

    Ball, Hole and tried Swing

--- src/main.rs
+++ src/main.rs
@@ -6,19 +6,15 @@ use bevy::{
     sprite::MaterialMesh2dBundle,
 };
 
-// These constants are defined in `Transform` units.
-// Using the default 2D camera they correspond 1:1 with screen pixels.
-const PADDLE_SIZE: Vec3 = Vec3::new(120.0, 20.0, 0.0);
-const GAP_BETWEEN_PADDLE_AND_FLOOR: f32 = 60.0;
-const PADDLE_SPEED: f32 = 500.0;
-// How close can the paddle get to the wall
-const PADDLE_PADDING: f32 = 10.0;
-
 // We set the z-value of the ball to 1 so it renders on top in the case of overlapping sprites.
 const BALL_STARTING_POSITION: Vec3 = Vec3::new(0.0, -50.0, 1.0);
-const BALL_SIZE: Vec3 = Vec3::new(30.0, 30.0, 0.0);
-const BALL_SPEED: f32 = 400.0;
+const BALL_SIZE: Vec3 = Vec3::new(15.0, 15.0, 0.0);
+const BALL_SPEED: f32 = 500.0;
 const INITIAL_BALL_DIRECTION: Vec2 = Vec2::new(0.5, -0.5);
+
+// We set the z-value of the ball to 1 so it renders on top in the case of overlapping sprites.
+const GOLF_HOLE_STARTING_POSITION: Vec3 = Vec3::new(0.0, 0.0, 0.0);
+const GOLF_HOLE_SIZE: Vec3 = Vec3::new(15.0, 15.0, 0.0);
 
 const WALL_THICKNESS: f32 = 10.0;
 // x coordinates
@@ -28,24 +24,24 @@ const RIGHT_WALL: f32 = 450.;
 const BOTTOM_WALL: f32 = -300.;
 const TOP_WALL: f32 = 300.;
 
-const BRICK_SIZE: Vec2 = Vec2::new(100., 30.);
-// These values are exact
-const GAP_BETWEEN_PADDLE_AND_BRICKS: f32 = 270.0;
-const GAP_BETWEEN_BRICKS: f32 = 5.0;
-// These values are lower bounds, as the number of bricks is computed
-const GAP_BETWEEN_BRICKS_AND_CEILING: f32 = 20.0;
-const GAP_BETWEEN_BRICKS_AND_SIDES: f32 = 20.0;
-
 const SCOREBOARD_FONT_SIZE: f32 = 40.0;
 const SCOREBOARD_TEXT_PADDING: Val = Val::Px(5.0);
 
-const BACKGROUND_COLOR: Color = Color::rgb(0.9, 0.9, 0.9);
-const PADDLE_COLOR: Color = Color::rgb(0.3, 0.3, 0.7);
-const BALL_COLOR: Color = Color::rgb(1.0, 0.5, 0.5);
-const BRICK_COLOR: Color = Color::rgb(0.5, 0.5, 1.0);
+const BACKGROUND_COLOR: Color = Color::rgb(0.0, 0.533333, 0.329412);
+const BALL_COLOR: Color = Color::WHITE;
+const GOLF_HOLE_COLOR: Color = Color::BLACK;
+
+// const BRICK_COLOR: Color = Color::rgb(0.5, 0.5, 1.0);
 const WALL_COLOR: Color = Color::rgb(0.8, 0.8, 0.8);
 const TEXT_COLOR: Color = Color::rgb(0.5, 0.5, 1.0);
 const SCORE_COLOR: Color = Color::rgb(1.0, 0.5, 0.5);
+
+#[derive(Clone, Copy, PartialEq, Eq, Hash, Debug, Default, States)]
+enum AppState {
+    #[default]
+    DeadBall,
+    BallMoving,
+}
 
 fn main() {
     App::new()
@@ -54,28 +50,33 @@ fn main() {
         .insert_resource(ClearColor(BACKGROUND_COLOR))
         .add_event::<CollisionEvent>()
         .add_systems(Startup, setup)
+        .add_state::<AppState>()
         // Add our gameplay simulation systems to the fixed timestep schedule
         // which runs at 64 Hz by default
+        // .add_systems(OnEnter(AppState::DeadBall), setup_swing)
+        .add_systems(
+            FixedUpdate,
+            (set_ball_velocity,).run_if(in_state(AppState::DeadBall)),
+        )
         .add_systems(
             FixedUpdate,
             (
                 apply_velocity,
-                move_paddle,
-                check_for_collisions,
-                play_collision_sound,
+                uptade_ball_velocity,
+                // play_collision_sound,
             )
-                // `chain`ing systems together runs them in order
-                .chain(),
+                .chain()
+                .run_if(in_state(AppState::BallMoving)), // `chain`ing systems together runs them in order
         )
-        .add_systems(Update, (update_scoreboard, bevy::window::close_on_esc))
+        .add_systems(Update, bevy::window::close_on_esc)
         .run();
 }
 
 #[derive(Component)]
-struct Paddle;
+struct Ball;
 
 #[derive(Component)]
-struct Ball;
+struct GolfHole;
 
 #[derive(Component, Deref, DerefMut)]
 struct Velocity(Vec2);
@@ -181,30 +182,42 @@ fn setup(
     commands.spawn(Camera2dBundle::default());
 
     // Sound
-    let ball_collision_sound = asset_server.load("sounds/breakout_collision.ogg");
-    commands.insert_resource(CollisionSound(ball_collision_sound));
+    // let ball_collision_sound = asset_server.load("sounds/breakout_collision.ogg");
+    // commands.insert_resource(CollisionSound(ball_collision_sound));
 
-    // Paddle
-    let paddle_y = BOTTOM_WALL + GAP_BETWEEN_PADDLE_AND_FLOOR;
+    // // Paddle
+    // let paddle_y = BOTTOM_WALL + GAP_BETWEEN_PADDLE_AND_FLOOR;
 
-    commands.spawn((
-        SpriteBundle {
-            transform: Transform {
-                translation: Vec3::new(0.0, paddle_y, 0.0),
-                scale: PADDLE_SIZE,
-                ..default()
-            },
-            sprite: Sprite {
-                color: PADDLE_COLOR,
-                ..default()
-            },
-            ..default()
-        },
-        Paddle,
-        Collider,
-    ));
+    // commands.spawn((
+    //     SpriteBundle {
+    //         transform: Transform {
+    //             translation: Vec3::new(0.0, paddle_y, 0.0),
+    //             scale: PADDLE_SIZE,
+    //             ..default()
+    //         },
+    //         sprite: Sprite {
+    //             color: PADDLE_COLOR,
+    //             ..default()
+    //         },
+    //         ..default()
+    //     },
+    //     Paddle,
+    //     Collider,
+    // ));
 
     // Ball
+
+    commands.spawn((
+        MaterialMesh2dBundle {
+            mesh: meshes.add(shape::Circle::default().into()).into(),
+            material: materials.add(ColorMaterial::from(GOLF_HOLE_COLOR)),
+            transform: Transform::from_translation(GOLF_HOLE_STARTING_POSITION)
+                .with_scale(GOLF_HOLE_SIZE),
+            ..default()
+        },
+        GolfHole,
+    ));
+
     commands.spawn((
         MaterialMesh2dBundle {
             mesh: meshes.add(shape::Circle::default().into()).into(),
@@ -247,87 +260,59 @@ fn setup(
     commands.spawn(WallBundle::new(WallLocation::Bottom));
     commands.spawn(WallBundle::new(WallLocation::Top));
 
-    // Bricks
-    let total_width_of_bricks = (RIGHT_WALL - LEFT_WALL) - 2. * GAP_BETWEEN_BRICKS_AND_SIDES;
-    let bottom_edge_of_bricks = paddle_y + GAP_BETWEEN_PADDLE_AND_BRICKS;
-    let total_height_of_bricks = TOP_WALL - bottom_edge_of_bricks - GAP_BETWEEN_BRICKS_AND_CEILING;
+    // // Bricks
+    // let total_width_of_bricks = (RIGHT_WALL - LEFT_WALL) - 2. * GAP_BETWEEN_BRICKS_AND_SIDES;
+    // let bottom_edge_of_bricks = paddle_y + GAP_BETWEEN_PADDLE_AND_BRICKS;
+    // let total_height_of_bricks = TOP_WALL - bottom_edge_of_bricks - GAP_BETWEEN_BRICKS_AND_CEILING;
 
-    assert!(total_width_of_bricks > 0.0);
-    assert!(total_height_of_bricks > 0.0);
+    // assert!(total_width_of_bricks > 0.0);
+    // assert!(total_height_of_bricks > 0.0);
 
-    // Given the space available, compute how many rows and columns of bricks we can fit
-    let n_columns = (total_width_of_bricks / (BRICK_SIZE.x + GAP_BETWEEN_BRICKS)).floor() as usize;
-    let n_rows = (total_height_of_bricks / (BRICK_SIZE.y + GAP_BETWEEN_BRICKS)).floor() as usize;
-    let n_vertical_gaps = n_columns - 1;
+    // // Given the space available, compute how many rows and columns of bricks we can fit
+    // let n_columns = (total_width_of_bricks / (BRICK_SIZE.x + GAP_BETWEEN_BRICKS)).floor() as usize;
+    // let n_rows = (total_height_of_bricks / (BRICK_SIZE.y + GAP_BETWEEN_BRICKS)).floor() as usize;
+    // let n_vertical_gaps = n_columns - 1;
 
-    // Because we need to round the number of columns,
-    // the space on the top and sides of the bricks only captures a lower bound, not an exact value
-    let center_of_bricks = (LEFT_WALL + RIGHT_WALL) / 2.0;
-    let left_edge_of_bricks = center_of_bricks
-        // Space taken up by the bricks
-        - (n_columns as f32 / 2.0 * BRICK_SIZE.x)
-        // Space taken up by the gaps
-        - n_vertical_gaps as f32 / 2.0 * GAP_BETWEEN_BRICKS;
+    // // Because we need to round the number of columns,
+    // // the space on the top and sides of the bricks only captures a lower bound, not an exact value
+    // let center_of_bricks = (LEFT_WALL + RIGHT_WALL) / 2.0;
+    // let left_edge_of_bricks = center_of_bricks
+    //     // Space taken up by the bricks
+    //     - (n_columns as f32 / 2.0 * BRICK_SIZE.x)
+    //     // Space taken up by the gaps
+    //     - n_vertical_gaps as f32 / 2.0 * GAP_BETWEEN_BRICKS;
 
-    // In Bevy, the `translation` of an entity describes the center point,
-    // not its bottom-left corner
-    let offset_x = left_edge_of_bricks + BRICK_SIZE.x / 2.;
-    let offset_y = bottom_edge_of_bricks + BRICK_SIZE.y / 2.;
+    // // In Bevy, the `translation` of an entity describes the center point,
+    // // not its bottom-left corner
+    // let offset_x = left_edge_of_bricks + BRICK_SIZE.x / 2.;
+    // let offset_y = bottom_edge_of_bricks + BRICK_SIZE.y / 2.;
 
-    for row in 0..n_rows {
-        for column in 0..n_columns {
-            let brick_position = Vec2::new(
-                offset_x + column as f32 * (BRICK_SIZE.x + GAP_BETWEEN_BRICKS),
-                offset_y + row as f32 * (BRICK_SIZE.y + GAP_BETWEEN_BRICKS),
-            );
+    // for row in 0..n_rows {
+    //     for column in 0..n_columns {
+    //         let brick_position = Vec2::new(
+    //             offset_x + column as f32 * (BRICK_SIZE.x + GAP_BETWEEN_BRICKS),
+    //             offset_y + row as f32 * (BRICK_SIZE.y + GAP_BETWEEN_BRICKS),
+    //         );
 
-            // brick
-            commands.spawn((
-                SpriteBundle {
-                    sprite: Sprite {
-                        color: BRICK_COLOR,
-                        ..default()
-                    },
-                    transform: Transform {
-                        translation: brick_position.extend(0.0),
-                        scale: Vec3::new(BRICK_SIZE.x, BRICK_SIZE.y, 1.0),
-                        ..default()
-                    },
-                    ..default()
-                },
-                Brick,
-                Collider,
-            ));
-        }
-    }
-}
-
-fn move_paddle(
-    keyboard_input: Res<Input<KeyCode>>,
-    mut query: Query<&mut Transform, With<Paddle>>,
-    time: Res<Time>,
-) {
-    let mut paddle_transform = query.single_mut();
-    let mut direction = 0.0;
-
-    if keyboard_input.pressed(KeyCode::Left) {
-        direction -= 1.0;
-    }
-
-    if keyboard_input.pressed(KeyCode::Right) {
-        direction += 1.0;
-    }
-
-    // Calculate the new horizontal paddle position based on player input
-    let new_paddle_position =
-        paddle_transform.translation.x + direction * PADDLE_SPEED * time.delta_seconds();
-
-    // Update the paddle position,
-    // making sure it doesn't cause the paddle to leave the arena
-    let left_bound = LEFT_WALL + WALL_THICKNESS / 2.0 + PADDLE_SIZE.x / 2.0 + PADDLE_PADDING;
-    let right_bound = RIGHT_WALL - WALL_THICKNESS / 2.0 - PADDLE_SIZE.x / 2.0 - PADDLE_PADDING;
-
-    paddle_transform.translation.x = new_paddle_position.clamp(left_bound, right_bound);
+    //         // brick
+    //         commands.spawn((
+    //             SpriteBundle {
+    //                 sprite: Sprite {
+    //                     color: BRICK_COLOR,
+    //                     ..default()
+    //                 },
+    //                 transform: Transform {
+    //                     translation: brick_position.extend(0.0),
+    //                     scale: Vec3::new(BRICK_SIZE.x, BRICK_SIZE.y, 1.0),
+    //                     ..default()
+    //                 },
+    //                 ..default()
+    //             },
+    //             Brick,
+    //             Collider,
+    //         ));
+    //     }
+    // }
 }
 
 fn apply_velocity(mut query: Query<(&mut Transform, &Velocity)>, time: Res<Time>) {
@@ -337,17 +322,53 @@ fn apply_velocity(mut query: Query<(&mut Transform, &Velocity)>, time: Res<Time>
     }
 }
 
-fn update_scoreboard(scoreboard: Res<Scoreboard>, mut query: Query<&mut Text>) {
-    let mut text = query.single_mut();
-    text.sections[1].value = scoreboard.score.to_string();
+// fn setup_swing(
+//     mut commands: Commands,
+//     mut meshes: ResMut<Assets<Mesh>>,
+//     mut materials: ResMut<Assets<ColorMaterial>>,
+//     asset_server: Res<AssetServer>,
+// ) {
+//     commands.spawn((
+//         MaterialMesh2dBundle {
+//             mesh: meshes.add(shape::Circle::default().into()).into(),
+//             material: materials.add(ColorMaterial::from(GOLF_HOLE_COLOR)),
+//             transform: Transform::from_translation(GOLF_HOLE_STARTING_POSITION)
+//                 .with_scale(GOLF_HOLE_SIZE),
+//             ..default()
+//         },
+//         GolfHole,
+//     ));
+// }
+
+fn set_ball_velocity(
+    mouse_input: Res<Input<MouseButton>>,
+    mut ball_query: Query<(&mut Velocity, & Transform), With<Ball>>,
+    mut app_state_next_state: ResMut<NextState<AppState>>,
+    mut window_query: Query<&mut Window>
+) {
+    let (mut ball_velocity, ball_transform) = ball_query.single_mut();
+    let cursor_position = window_query.single_mut().physical_cursor_position().unwrap_or(Vec2::new(0.0, 0.0));
+
+    let ball_position = ball_transform.translation;
+    let ball_velocity_vector = Vec2::new(
+            cursor_position.x - ball_position.x,
+            cursor_position.y - ball_position.y
+    );
+
+    // println!("{} {}", ball_velocity_vector.x, ball_velocity_vector.y);
+
+    if mouse_input.pressed(MouseButton::Left) {
+        *ball_velocity = Velocity(ball_velocity_vector);
+        app_state_next_state.set(AppState::BallMoving);
+        println!("Entered AppState::BallMoving");
+    }
 }
 
-fn check_for_collisions(
-    mut commands: Commands,
-    mut scoreboard: ResMut<Scoreboard>,
+fn uptade_ball_velocity(
     mut ball_query: Query<(&mut Velocity, &Transform), With<Ball>>,
     collider_query: Query<(Entity, &Transform, Option<&Brick>), With<Collider>>,
     mut collision_events: EventWriter<CollisionEvent>,
+    mut app_state_next_state: ResMut<NextState<AppState>>,
 ) {
     let (mut ball_velocity, ball_transform) = ball_query.single_mut();
     let ball_size = ball_transform.scale.truncate();
@@ -363,12 +384,6 @@ fn check_for_collisions(
         if let Some(collision) = collision {
             // Sends a collision event so that other systems can react to the collision
             collision_events.send_default();
-
-            // Bricks should be despawned and increment the scoreboard on collision
-            if maybe_brick.is_some() {
-                scoreboard.score += 1;
-                commands.entity(collider_entity).despawn();
-            }
 
             // reflect the ball when it collides
             let mut reflect_x = false;
@@ -395,22 +410,33 @@ fn check_for_collisions(
             }
         }
     }
-}
 
-fn play_collision_sound(
-    mut commands: Commands,
-    mut collision_events: EventReader<CollisionEvent>,
-    sound: Res<CollisionSound>,
-) {
-    // Play a sound once per frame if a collision occurred.
-    if !collision_events.is_empty() {
-        // This prevents events staying active on the next frame.
-        collision_events.clear();
-        commands.spawn(AudioBundle {
-            source: sound.0.clone(),
-            // auto-despawn the entity when playback finishes
-            settings: PlaybackSettings::DESPAWN,
-        });
+    if ball_velocity.x.abs() == 0.0 && ball_velocity.y.abs() == 0.0 {
+        app_state_next_state.set(AppState::DeadBall);
+        println!("Entered AppState::DeadBall");
+    } else {
+        if ball_velocity.x.abs() >= 0.0 {
+            ball_velocity.x -= ball_velocity.x.signum();
+        }
+        if ball_velocity.y.abs() >= 0.0 {
+            ball_velocity.y -= ball_velocity.y.signum();
+        }
     }
 }
 
+// fn play_collision_sound(
+//     mut commands: Commands,
+//     mut collision_events: EventReader<CollisionEvent>,
+//     sound: Res<CollisionSound>,
+// ) {
+//     // Play a sound once per frame if a collision occurred.
+//     if !collision_events.is_empty() {
+//         // This prevents events staying active on the next frame.
+//         collision_events.clear();
+//         commands.spawn(AudioBundle {
+//             source: sound.0.clone(),
+//             // auto-despawn the entity when playback finishes
+//             settings: PlaybackSettings::DESPAWN,
+//         });
+//     }
+// }

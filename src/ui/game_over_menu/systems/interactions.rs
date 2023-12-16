9fe01dd2cb132ae8dbd41155959e0944026fddad
4NTEDEGUEM0N <solbrilhan@gmail.com>
2023-12-16 14:35:41 -0300

    button game mouse click fix

--- src/ui/game_over_menu/systems/interactions.rs
+++ src/ui/game_over_menu/systems/interactions.rs
@@ -5,7 +5,27 @@ use crate::ui::game_over_menu::components::*;
 use crate::ui::game_over_menu::styles::*;
 use crate::AppState;
 
+#[derive(Resource)]
+pub struct RestartClicked {
+    pub value: bool,
+}
+
+impl Default for RestartClicked {
+    fn default() -> RestartClicked {
+        RestartClicked {value: false}
+    }
+}
+
+pub fn insert_restart_clicked(mut commands: Commands) {
+    commands.insert_resource(RestartClicked::default())
+}
+
+pub fn remove_restart_clicked(mut commands: Commands) {
+    commands.remove_resource::<RestartClicked>();
+}
+
 pub fn interact_with_restart_button(
+    mut restart_clicked: ResMut<RestartClicked>,
     mut button_query: Query<
         (&Interaction, &mut BackgroundColor),
         (Changed<Interaction>, With<RestartButton>),
@@ -15,14 +35,18 @@ pub fn interact_with_restart_button(
     for (interaction, mut color) in button_query.iter_mut() {
         match *interaction {
             Interaction::Pressed => {
+                restart_clicked.value = true;
                 *color = PRESSED_BUTTON.into();
-                app_state_next_state.set(AppState::Game);
-                println!("Entered AppState::Game");
             }
             Interaction::Hovered => {
+                if restart_clicked.value {
+                    app_state_next_state.set(AppState::Game);
+                    println!("Entered AppState::Game");
+                }
                 *color = HOVERED_BUTTON.into();
             }
             Interaction::None => {
+                restart_clicked.value = false;
                 *color = NORMAL_BUTTON.into();
             }
         }
